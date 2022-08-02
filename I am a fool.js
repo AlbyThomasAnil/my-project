3dbf772dacbba8cb85213f68eea953a70364edda
AlbyThomasAnil <71020580+AlbyThomasAnil@users.noreply.github.com>
2022-08-02 11:51:57 +0400

    Add files via upload

---
+++ I am a fool.js
@@ -0,0 +1,9 @@
+function myFunction1(){
+    document.getElementById("demo1").innerHTML="No"
+    document.getElementById("demo2").innerHTML="Yes";
+    }
+    function myFunction12(){
+    document.getElementById("demo1").innerHTML="Yes";
+    document.getElementById("demo2").innerHTML="No";
+    }
+    
